635409ccc500c43bc99f26eb8628e5db75e81b91
johnware15 <contact@johndware.com>
2018-01-10 13:32:03 -0800

    Build server with imports and express

--- src/server.js
+++ src/server.js
@@ -2,4 +2,26 @@ import express from 'express'
 import session from 'express-session'
 import bodyParser from 'body-parser'
 import pgp from 'pg-promise'
-import override from 'method-override'
+import methodOverride from 'method-override'
+
+const app = express()
+const routes = require('./controller/routes');
+const db = require('./model/db');
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
+
+app.use(express.static('public'))
+
+app.use(methodOverride('method'))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+module.exports = app
