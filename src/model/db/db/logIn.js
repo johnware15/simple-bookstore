f6831abf6f0e9be1310e715c1511309a532465c6
johnware15 <contact@johndware.com>
2018-01-12 16:21:50 -0800

    Switch from HTML to EJS for view engine.

--- src/model/db/db/logIn.js
+++ src/model/db/db/logIn.js
@@ -1,3 +1,5 @@
+import db from '../connection.js'
+
 function logIn() {
   db.oneOrNone(`SELECT * FROM users WHERE email=$1 AND password=$2`, [email, password])
     .then(result => result.rows)
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 'use strict'
+import path from 'path'
 import express from 'express'
 import session from 'express-session'
 import bodyParser from 'body-parser'
@@ -7,12 +8,17 @@ import methodOverride from 'method-override'
 
 import logInRoute from '../src/controller/routes/logInRoute.js'
 import signUpRoute from '../src/controller/routes/signUpRoute.js'
+import logIn from '../src/model/db/db/logIn.js'
+import signUp from '../src/model/db/db/signUp.js'
+
 const app = express()
 // const routes = require('./src/controller/routes');
 // const db = require('./model/db');
 const port = process.env.PORT || 3000
 
-app.set('view engine', 'html')
+const ROOT_DIR = path.resolve(__dirname, '../')
+
+app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 
 app.listen(port, () => {
@@ -23,13 +29,22 @@ app.use(express.static('public'))
 
 app.use(methodOverride('method'))
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
+
+app.use(express.static(`${ROOT_DIR}/public`))
 
 app.use(signUpRoute)
 
 app.use(logInRoute)
 
+app.use(signUp)
+
+app.use(logIn)
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 module.exports = app
